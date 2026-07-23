83e439e35f12c5e7e88dbc454125485a37ef47d3
sanjay purty <rpurty582@gmail.com>
2026-07-23 09:04:59 +0530

    update mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://sanjay:7FPK6Ij9tY6HBslJ@cluster0.71hrmfe.mongodb.net/?appName=Cluster0")
+mongoose.connect("mongodb+srv://sanjay:sanjay12345@cluster0.71hrmfe.mongodb.net/?appName=Cluster0")
